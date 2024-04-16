21f19d393a5f43bde12ac2e505ad9fd930a7b7e0
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-04-16 20:17:39 +0200

    najlepiej dziala

--- Lista4/zad1/gui/App.java
+++ Lista4/zad1/gui/App.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.BoxLayout;
+
 import trojkatpascala.WierszTrojkataPascala;
 
 class BasicWindowAdapter extends WindowAdapter {
@@ -26,8 +28,9 @@ public class App {
         MainFrame mf = new MainFrame();
         mf.Initialise();
 
-
         Panel buttonPanel = new Panel();
+        buttonPanel.setPreferredSize(new Dimension(300, 40));
+        buttonPanel.setBackground(new Color(5, 84, 242));
 
         TextField inputField = new TextField();
         Button makeButton = new Button("Stworz Trojkat");
@@ -35,18 +38,35 @@ public class App {
         TrianglePanel tPanel = new TrianglePanel();
         
         makeButton.addActionListener(new ActionListener() {
+            Label lastErrorLabel;
+
             public void actionPerformed(ActionEvent e) {
-                tPanel.showTriangle(Integer.parseInt(inputField.getText()));
+                if(lastErrorLabel!=null)
+                {
+                    buttonPanel.remove(lastErrorLabel);
+                }
+                try
+                {
+                    tPanel.showTriangle(Integer.parseInt(inputField.getText()));
+                }
+                catch(NumberFormatException ex)
+                {
+                    lastErrorLabel = new Label("Tylko liczby całkowite!");
+                    lastErrorLabel.setBackground(new Color(242, 5, 5));
+                    buttonPanel.add(lastErrorLabel);
+                }
                 mf.pack();
             }
         });
         
+        tPanel.showTriangle(5);
         buttonPanel.add(inputField);
         buttonPanel.add(makeButton);
         mf.add(buttonPanel);
         mf.add(tPanel);
         
-        mf.setLayout(new GridLayout(2, 1));
+        mf.setLayout(new BoxLayout(mf, 1));
         mf.setVisible(true);
+        mf.pack();
     }
 }
--- Lista4/zad1/gui/TrianglePanel.java
+++ Lista4/zad1/gui/TrianglePanel.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Panel;
@@ -12,8 +13,7 @@ public class TrianglePanel extends Panel{
     public TrianglePanel()
     {
         //super(ScrollPane.SCROLLBARS_ALWAYS);
-        this.setBounds(100, 100, 400, 400);
-        this.setBackground(new Color(50));
+        // this.setBackground(new Color(50));
     }
 
     public void showTriangle(int triangleSize)
@@ -21,14 +21,14 @@ public class TrianglePanel extends Panel{
         if(insidePanel!=null)
             this.remove(insidePanel);
         insidePanel = new Panel();
-        insidePanel.setBackground(new Color(123));
+        insidePanel.setBackground(new Color(242, 242, 242));
         insidePanel.setLayout(new GridLayout(triangleSize, 1));
 
         for(int i=0;i<triangleSize;i++)
         {
             PascalLabelRow testRow = new PascalLabelRow(i);
             testRow.Initialise();
-
+            testRow.setFont(new Font(getName(), 0+2*(i%2), 12));
             insidePanel.add(testRow);
         }
 
