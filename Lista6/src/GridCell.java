09c884a25f4aed9f702107d81d94364d6f27efe8
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-05-16 21:17:28 +0200

    GridCell init

---
+++ Lista6/src/GridCell.java
@@ -0,0 +1,13 @@
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class GridCell extends Pane {
+    public GridCell(Color color) {
+        this.setMinSize(50, 50);
+        this.setBackground(new Background(
+                new BackgroundFill(color, null, new Insets(5))));
+    }
+}
--- Lista6/src/MGridPane.java
+++ Lista6/src/MGridPane.java
@@ -1,29 +1,22 @@
-import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class MGridPane extends GridPane {
-    Pane[][] panes;
+    GridCell[][] cells;
 
     public MGridPane(int n, int m, int k, int p) {
         super(n, m);
         
-        panes = new Pane[n][m];
+        cells = new GridCell[n][m];
         CreatePanes(n, m);
     }
 
     private void CreatePanes(int n, int m) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                Pane pan = new Pane();
-                pan.setMinSize(50, 50);
-                pan.setBackground(new Background(
-                        new BackgroundFill(Color.rgb(m + 100, (i * 20) % 255, (j * 30) % 255), null, new Insets(5))));
-                panes[i][j] = pan;
-                this.add(pan, i, j);
+                GridCell cell = new GridCell(Color.rgb(m + 100, (i * 20) % 255, (j * 30) % 255));
+                cells[i][j] = cell;
+                this.add(cell, i, j);
             }
         }
     }
