d25b8321db65ddfcb3d24a5d8eae7b862e9384d7
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-05-16 20:18:35 +0200

    MGridPane refactor

--- Lista6/src/MGridPane.java
+++ Lista6/src/MGridPane.java
@@ -5,16 +5,24 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
-public class MGridPane extends GridPane{
-        public MGridPane(int n, int m, int k, int p) {
+public class MGridPane extends GridPane {
+    Pane[][] panes;
+
+    public MGridPane(int n, int m, int k, int p) {
         super(n, m);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
+        
+        panes = new Pane[n][m];
+        CreatePanes(n, m);
+    }
+
+    private void CreatePanes(int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 Pane pan = new Pane();
                 pan.setMinSize(50, 50);
-                pan.setBackground(new Background(new BackgroundFill(Color.rgb(m+100, (i*20) % 255, (j*30)%255), null, new Insets(5))));
+                pan.setBackground(new Background(
+                        new BackgroundFill(Color.rgb(m + 100, (i * 20) % 255, (j * 30) % 255), null, new Insets(5))));
+                panes[i][j] = pan;
                 this.add(pan, i, j);
             }
         }
