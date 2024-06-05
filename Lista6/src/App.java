13a8a35d5df6e6a12b8058070d6306a5f5cf3690
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-06-05 15:08:33 +0200

    docs

--- Lista6/src/App.java
+++ Lista6/src/App.java
@@ -1,10 +1,11 @@
 import javafx.application.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 /**
  * Głowna klasa uruchamiająca aplikację
  */
-public class App extends Application{
+public class App extends Application {
     private static int n = 6;
     private static int m = 10;
     private static double k = 100;
@@ -13,7 +14,9 @@ public class App extends Application{
     private MGridPane grid;
 
     /**
-     * @param args lista arguemntów: szerokość, wysokość, interwał, szansa na zmianę
+     * Funkcja main
+     * 
+     * @param args lista argumentów: szerokość, wysokość, interwał, szansa na zmianę
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
@@ -22,37 +25,36 @@ public class App extends Application{
             m = Integer.parseInt(args[1]);
             k = Double.parseDouble(args[2]);
             p = Double.parseDouble(args[3]);
-            if(n <= 0 || m <= 0 || k <= 0 || p <= 0) {
+            if (n <= 0 || m <= 0 || k <= 0 || p <= 0) {
                 System.out.println("Argumenty muszą być liczbami dodatnimi");
                 return;
             }
-        }
-        catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println("Dane muszą być liczbami");
             return;
-        }
-        catch(IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Za mało argumentów");
             // return; TODO: ADD RETURN
         }
         launch(args);
     }
+
     /**
      * Metoda tworząca scenę oraz grid, a następnie uruchamiająca wątki
      */
     @Override
     public void start(Stage stage) throws Exception {
-        grid = new MGridPane(n,m, k, p);
+        grid = new MGridPane(n, m, k, p);
         grid.setPrefSize(400, 400);
-        
+
         Scene scene = new Scene(grid);
-        
+
         stage.setScene(scene);
-        stage.show();   
+        stage.show();
         grid.startThreads();
     }
 
-    //Zatrzymanie pracy wątków po zamknięciu okna
+    // Zatrzymanie pracy wątków po zamknięciu okna
     @Override
     public void stop() {
         grid.stopThreads();
