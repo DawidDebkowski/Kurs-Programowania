cd2b48fac7956c3ecdec76e188eaff9a46ac434e
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-06-10 16:03:23 +0200

    search iterative

--- Lista7/src/BinaryTree.java
+++ Lista7/src/BinaryTree.java
@@ -2,35 +2,47 @@ public class BinaryTree<T extends Comparable<T>> {
     private Node<T> rootNode = null;
 
     public BinaryTree() {
-
+    }
+    //TODO zmienić if () na if()
+    public BinaryTree(T rootKey) {
+        insert(new Node<T>(rootKey, null));
     }
 
-    public BinaryTree(T rootValue) {
-        rootNode = new Node<T>(rootValue, null);
-    }
+    public Node<T> search(Node<T> startNode, T key) {
+        if (startNode == null) {
+            return null;
+        }
 
-    public T search() {
-        return null;
+        Node<T> head = startNode;
+        while (head.key != key) {
+            if (head.key.compareTo(key) < 0) {
+                head = head.left;
+            } else
+                head = head.right;
+
+            if(head == null) {return head;}
+        }
+        return head;
     }
 
     public void insert(Node<T> node) {
         Node<T> parent = null;
         Node<T> searchNode = rootNode;
-        
-        //szukanie miejsca na węzeł
+
+        // szukanie miejsca na węzeł
         while (searchNode != null) {
             parent = searchNode;
-            if (searchNode.value.compareTo(node.value) < 0) {
+            if (searchNode.key.compareTo(node.key) < 0) {
                 searchNode = searchNode.left;
             } else
                 searchNode = searchNode.right;
         }
 
-        //podpięcie węzła
+        // podpięcie węzła
         node.parent = parent;
         if (parent == null) {
             rootNode = node;
-        } else if (parent.value.compareTo(node.value) < 0) {
+        } else if (parent.key.compareTo(node.key) < 0) {
             parent.left = node;
         } else {
             parent.right = node;
--- Lista7/src/Node.java
+++ Lista7/src/Node.java
@@ -2,10 +2,10 @@ public class Node<T extends Comparable<T>> {
     Node<T> left;
     Node<T> right;
     Node<T> parent;
-    final T value;
-    
-    public Node(T value, Node<T> parent) {
-        this.value = value;
+    final T key;
+
+    public Node(T key, Node<T> parent) {
+        this.key = key;
         this.parent = parent;
     }
 }
